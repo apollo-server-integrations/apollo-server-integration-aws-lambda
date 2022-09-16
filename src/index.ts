7962bbfe19af87f08843418d498d27ca10448f13
Trevor Scheer <trevor.scheer@gmail.com>
2022-09-16 15:02:45 -0700

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import type {
   ApolloServer,
   BaseContext,
   ContextFunction,
+  HTTPGraphQLRequest,
 } from '@apollo/server';
 import type { WithRequired } from '@apollo/utils.withrequired';
 import type {
@@ -54,36 +55,29 @@ export function lambdaHandler<TContext extends BaseContext>(
   > = options?.context ?? defaultContext;
 
   return async function (event, context) {
-    debugger;
     try {
       const normalizedEvent = normalizeGatewayEvent(event);
 
-      try {
-        const httpGraphQLResponse = await server.executeHTTPGraphQLRequest({
-          httpGraphQLRequest: normalizedEvent,
-          context: () => contextFunction({ event, context }),
-        });
+      const httpGraphQLResponse = await server.executeHTTPGraphQLRequest({
+        httpGraphQLRequest: normalizedEvent,
+        context: () => contextFunction({ event, context }),
+      });
 
-        if (httpGraphQLResponse.completeBody === null) {
-          throw Error('Incremental delivery not implemented');
-        }
-
-        return {
-          statusCode: httpGraphQLResponse.status || 200,
-          headers: {
-            ...Object.fromEntries(httpGraphQLResponse.headers),
-            'content-length': Buffer.byteLength(
-              httpGraphQLResponse.completeBody,
-            ).toString(),
-          },
-          body: httpGraphQLResponse.completeBody,
-        };
-      } catch (error) {
-        debugger;
-        throw error;
+      if (httpGraphQLResponse.completeBody === null) {
+        throw Error('Incremental delivery not implemented');
       }
+
+      return {
+        statusCode: httpGraphQLResponse.status || 200,
+        headers: {
+          ...Object.fromEntries(httpGraphQLResponse.headers),
+          'content-length': Buffer.byteLength(
+            httpGraphQLResponse.completeBody,
+          ).toString(),
+        },
+        body: httpGraphQLResponse.completeBody,
+      };
     } catch (e) {
-      debugger;
       return {
         statusCode: 400,
         body: (e as Error).message,
@@ -92,60 +86,16 @@ export function lambdaHandler<TContext extends BaseContext>(
   };
 }
 
-interface NormalizedGatewayEvent {
-  method: string;
-  headers: Map<string, string>;
-  search: string;
-  body: object | string;
-}
-
 function normalizeGatewayEvent(
   event: APIGatewayProxyEvent | APIGatewayProxyEventV2,
-): NormalizedGatewayEvent {
-  const method = isV1Event(event)
-    ? event.httpMethod
-    : isV2Event(event)
-    ? event.requestContext.http.method
-    : undefined;
-
-  if (!method) throw new Error('Unknown event type');
-
-  // TODO: restructure this logically
-  let parsedBody: object | string = '';
-  if (!event.body) {
-    // assert there's a query string?
-  } else if (event.headers['content-type'] === 'application/json') {
-    parsedBody = JSON.parse(event.body);
-  } else if (event.headers['content-type'] === 'text/plain') {
-    parsedBody = event.body;
+): HTTPGraphQLRequest {
+  if (isV1Event(event)) {
+    return normalizeV1Event(event);
+  } else if (isV2Event(event)) {
+    return normalizeV2Event(event);
+  } else {
+    throw Error('Unknown event type');
   }
-
-  const headers = new Map<string, string>();
-  for (const [key, value] of Object.entries(event.headers)) {
-    if (value !== undefined) {
-      headers.set(key, value);
-    }
-  }
-
-  const search = isV1Event(event)
-    ? Object.entries({
-        ...event.queryStringParameters,
-        ...event.multiValueQueryStringParameters,
-      }).reduce((queryString, next, i) => {
-        const delimiter = i === 0 ? '?' : '&';
-        const addition = Array.isArray(next[1])
-          ? next[1].map((v) => `${next[0]}=${v}`).join('&')
-          : `${next[0]}=${next[1]}`;
-        return `${queryString}${delimiter}${addition}`;
-      }, '')
-    : event.rawQueryString;
-
-  return {
-    method,
-    headers,
-    search,
-    body: parsedBody,
-  };
 }
 
 function isV1Event(
@@ -158,4 +108,61 @@ function isV2Event(
   event: APIGatewayProxyEvent | APIGatewayProxyEventV2,
 ): event is APIGatewayProxyEventV2 {
   return !isV1Event(event);
+}
+
+function normalizeV1Event(event: APIGatewayProxyEvent): HTTPGraphQLRequest {
+  const headers = normalizeHeaders(event.headers);
+  const body = parseBody(event.body, headers.get('content-type'));
+  const reconstructedSearchString = Object.entries({
+    ...event.queryStringParameters,
+    ...event.multiValueQueryStringParameters,
+  }).reduce((queryString, next, i) => {
+    const delimiter = i === 0 ? '?' : '&';
+    const addition = Array.isArray(next[1])
+      ? next[1].map((v) => `${next[0]}=${v}`).join('&')
+      : `${next[0]}=${next[1]}`;
+    return `${queryString}${delimiter}${addition}`;
+  }, '');
+
+  return {
+    method: event.httpMethod,
+    headers,
+    search: reconstructedSearchString,
+    body,
+  };
+}
+
+function normalizeV2Event(event: APIGatewayProxyEventV2): HTTPGraphQLRequest {
+  const headers = normalizeHeaders(event.headers);
+  return {
+    method: event.requestContext.http.method,
+    headers,
+    search: event.rawQueryString,
+    body: parseBody(event.body, headers.get('content-type')),
+  };
+}
+
+function parseBody(
+  body: string | null | undefined,
+  contentType: string | undefined,
+): object | string {
+  if (!body) {
+    return '';
+  } else if (contentType === 'application/json') {
+    return JSON.parse(body);
+  } else if (contentType === 'text/plain') {
+    return body;
+  } else {
+    return '';
+  }
+}
+
+function normalizeHeaders(
+  headers: Record<string, string | undefined>,
+): Map<string, string> {
+  const headerMap = new Map<string, string>();
+  for (const [key, value] of Object.entries(headers)) {
+    headerMap.set(key, value ?? '');
+  }
+  return headerMap;
 }
